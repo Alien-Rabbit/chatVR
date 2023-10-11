c19d9bf7448983c0b7ba40612796ce3648880505
drnoir <chrispgodber@gmail.com>
2023-10-11 18:17:34 +0100

    - work on multicam component

---
+++ ChatVR/js/multicam.component.js
@@ -0,0 +1,27 @@
+AFRAME.registerComponent('multicam', {
+    schema: {
+      speed: {default: 0.05},
+      id: {default: 1},
+    },
+  
+    init: function() {
+        this.angle = 0;
+        this.center = this.el.getAttribute('position');
+        const el = this.el;
+        const data = this.data;
+        const id = data.id;
+
+        const newMulticam = document.createElement('a-entity');
+        newMulticam.setAttribute('id', 'camera'+id)
+        newMulticam.setAttribute('secondary-camera', 'outputElement:#viewport'+id)
+        const pos = el.getAttribute('position')
+        newMulticam.object3D.position.set(pos);
+        el.appendChild(newMulticam);
+
+    },
+    tick: function() {
+    },
+    remove: function(){
+     this.el.destroy();
+    }
+  });
